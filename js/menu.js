1b43b03c6894d682fcde88adaa8aacdc3f7cb334
Laureano <ramonero27@gmail.com>
2024-07-01 16:38:08 -0300

    new changes in menu navbar

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,12 @@ const closeBtn = document.querySelector('.close');
 const menu = document.querySelector('.nav__menu');
 
 openBtn.addEventListener('click',()=>{
-    menu.style.left="0";
+    menu.style.right="0";
+    menu.style.animation="mostrar .5s ease-in both";
+    
 })
 
 closeBtn.addEventListener('click',()=>{
-    menu.style.left="-100%";
+    menu.style.right="-100%";
+    menu.style.animation="ocultar .5s ease-in both";
 });
